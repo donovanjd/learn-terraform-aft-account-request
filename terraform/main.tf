ff97d49dd141fd5ed045d67e54ce5f169c7ddfae
donovanjd <donovandurand@gmail.com>
2022-06-14 00:45:08 +1000

    update correct module name

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ module "sandbox" {
   account_customizations_name = "sandbox"
 }
 
-module "sandbox" {
+module "sandbox1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
